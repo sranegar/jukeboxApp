df280940abde22dc4a760abd74d53df6ab8c29ae
sranegar <stephanie.ranegar@gmail.com>
2022-11-26 05:27:44 -0500

    Upload file functionality

--- src/app/create/create.page.ts
+++ src/app/create/create.page.ts
@@ -1,24 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController, AlertController } from '@ionic/angular';
-import { FirestoreService } from '../services/data/firestore.service';
-
+import { FirestoreService  } from '../services/data/firestore.service';
+import {
+  
+  Firestore 
+  
+} from '@angular/fire/firestore';
+import { Song } from '../models/song.interface';
 @Component({
   selector: 'app-create',
   templateUrl: './create.page.html',
   styleUrls: ['./create.page.scss'],
 })
-export class CreatePage {
-  selectedFile: any;
+export class CreatePage implements OnInit {
   createSongForm: FormGroup;
+ 
+  selectedFile: any;
+  songsRef: any;
+ 
+  newFileName = '';
   constructor(
     private readonly loadingCtrl: LoadingController,
     private readonly alertCtrl: AlertController,
     private firestoreService: FirestoreService,
     formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+ 
   ) {
+    this.songsRef = (this.firestoreService, "songs");
     this.createSongForm = formBuilder.group({
       songName: ['', Validators.required],
       artistName: ['', Validators.required],
@@ -27,29 +38,54 @@ export class CreatePage {
     });
   }
 
- 
-  async createSong(song) {
-    const loading = await this.loadingCtrl.create();
-
+  ngOnInit() {}
+  chooseFile(event) {
+    this.selectedFile = event.target.files[0];
+    
+  }
+  
+  addNewSong() {
     const name = this.createSongForm.value.songName;
     const artist = this.createSongForm.value.artistName;
     const album = this.createSongForm.value.albumName;
     const file = this.createSongForm.value.fileName;
-    song = { name, artist, album, file };
+    const song = ({ name, artist, album, file });
+    this.firestoreService
+      .addSong(song)
+      .then((res) => {
+        this.firestoreService.uploadFile(
+          res.id,
+          this.selectedFile
+        );
 
-    this.firestoreService.createSong(song).then(
-      () => {
-        loading.dismiss().then(() => {
-          this.router.navigateByUrl('/home');
-        });
-      },
-      (error) => {
-        loading.dismiss().then(() => {
-          console.error(error);
-        });
-      }
-    );
-
-    return await loading.present();
+       
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
+  // async createSong(song) {
+  //   const loading = await this.loadingCtrl.create();
+
+  //   const name = this.createSongForm.value.songName;
+  //   const artist = this.createSongForm.value.artistName;
+  //   const album = this.createSongForm.value.albumName;
+  //   const file = this.createSongForm.value.fileName;
+  //   song = { name, artist, album, file };
+
+  //   this.firestoreService.addSong(song).then(
+  //     () => {
+  //       loading.dismiss().then(() => {
+  //         this.router.navigateByUrl('/home');
+  //       });
+  //     },
+  //     (error) => {
+  //       loading.dismiss().then(() => {
+  //         console.error(error);
+  //       });
+  //     }
+  //   );
+
+  //   return await loading.present();
+  // }
 }
--- src/app/services/data/firestore.service.ts
+++ src/app/services/data/firestore.service.ts
@@ -1,16 +1,10 @@
 import { Injectable } from '@angular/core';
 import { idToken } from '@angular/fire/auth';
 import {
-  
-  ref,
-  deleteObject,
-  uploadBytes,
-  uploadString,
-  uploadBytesResumable,
-  percentage,
-  getDownloadURL,
   Storage,
-  
+  ref,
+  getDownloadURL,
+  uploadBytes,
 } from '@angular/fire/storage';
 import {
   addDoc,
@@ -25,16 +19,33 @@ import {
  
 import { Observable } from 'rxjs';
 import { Song } from '../../models/song.interface';
+import { setDoc } from '@firebase/firestore';
  
 @Injectable({
   providedIn: 'root',
 })
 export class FirestoreService {
- 
-  constructor(private firestore: Firestore, private storage: Storage ) {}
+  fileName: string;
+  constructor(private firestore: Firestore, private storage: Storage) {}
 
- 
-  createSong(song: Song) {
+  async uploadFile(id: string, f) {
+    const fileName = f.name;
+    const path = `mp3/${fileName}`;
+    console.log(path)
+    const storageRef = ref(this.storage, path);
+    try {
+      await uploadBytes(storageRef, f);
+      const mp3Url = await getDownloadURL(storageRef);
+      const songDocRef = doc(this.firestore, `songs/${id}`);
+      await updateDoc(songDocRef, { file: mp3Url });
+      return true;
+    } catch (e) {
+      console.log('Upload mp3 file error:', e);
+      return null;
+    }
+  }
+
+  addSong(song: Song) {
     const songsRef = collection(this.firestore, 'songs');
     return addDoc(songsRef, song);
   }
@@ -55,8 +66,12 @@ export class FirestoreService {
     return deleteDoc(songDocRef);
   }
   updateSong(id, song: Song) {
-    const songsRef = doc(this.firestore, `songs/${id}`)
-    return updateDoc(songsRef, {name: song.name, artist: song.artist, album: song.album, file: song.file})
-    
+    const songsRef = doc(this.firestore, `songs/${id}`);
+    return updateDoc(songsRef, {
+      name: song.name,
+      artist: song.artist,
+      album: song.album,
+      file: song.file,
+    });
   }
 }
